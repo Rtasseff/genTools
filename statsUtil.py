3d342317edb5eb94634273fb250fde8b6cad2ff0
rtasseff <rtasseff@gmail.com>
2014-11-18 14:13:49 +0100

    added some very basic plots for pariwise analysis NN NC CC

--- statsUtil.py
+++ statsUtil.py
@@ -25,6 +25,7 @@ import scipy.stats as stats
 import scipy.linalg
 import operator as op
 import itertools
+import matplotlib.pyplot as plt
 
 def iqr(x):
 	"""returns the inter quantile distance for
@@ -612,4 +613,99 @@ def runPairwise(x,y,xType, yType, obsMinWarn=5, obsMinError=1):
 
 	
 	
+def plotPairwise(x,y,varType=['',''],varName=['',''],outfile=''):
+	"""Diffrent combinations of variable types
+	are visulized by diffrent plots, this funciton
+	uses a basic visulization for the correct combinaiton.
+	x	np str array, if type = N then this should be 
+		convertable to float array
+	y	same as x but for other variable
+	varType	tuple with 2 values to indicate type of x and y 
+		if left blank we assume x and y are lines form a 
+		feature matrix and the first entry is the label
+	"""
+	if varType[0]=='':
+		varType[0] = x[0].split(':')[0]
+		varName[0] = x[0].split(':')[-1]
+		x = x[1:]
+		varType[1] = y[0].split(':')[0]
+		varName[1] = y[0].split(':')[-1]
+		y = y[1:]
+
+	if not varType[0]=='N' and not varType[0]=='C' and not varType[0]=='B':
+		raise ValueError( 'Variable type for x is unknwon: '+varType[0])
+	if not varType[1]=='N' and not varType[1]=='C' and not varType[1]=='B':
+		raise ValueError( 'Variable type for y is unknwon: '+varType[1])
+
+
+	if varType[0] == 'B': varType[0] = 'C' # no diff here
+	if varType[1] == 'B': varType[1] = 'C' # no diff here
+
+	# check if both numerical:
+	if varType[0]=='N' and varType[1]=='N':
+		# scatter plot with line from least squares
+		A = np.vstack([x, np.ones(len(x))]).T
+		m, c = np.linalg.lstsq(A, y)[0]
+
+		plt.plot(np.array(x,dtype=float),np.array(y,dtype=float),'o',label='Data')
+		plt.plot(x, m*x + c, 'r', label='Fitted line')
+		plt.legend()
+
+		plt.xlabel(varName[0])
+		plt.ylabel(varName[1])
+	elif varType[0]=='C' and varType[1]=='C':
+		# right now we are just going with a simple 
+		# stacked bar plots, more advanced things
+		# like mosics are avalible in R or pythons statsmodels
+
+		conTable,xCat,yCat = makeConTable(x,y)
+		n,m = conTable.shape
+		ind = np.arange(m)
+		bottomTmp = np.zeros(m)
+		colorList = ['b','g','r','c','y','m']
+		nColor = len(colorList)
+		colorInd = 0
+		for i in range(n):
+			plt.bar(ind,conTable[i],bottom=bottomTmp,color=colorList[colorInd],label=varName[0]+'::'+str(xCat[i]))
+			bottomTmp = conTable[i]
+			colorInd +=1
+			if colorInd==nColor:colorInd=0
+
+		plt.ylabel('Count')
+		plt.xlabel(varName[1])
+		plt.xticks(ind,np.array(yCat,dtype=str),rotation=45)
+		plt.legend()
+			
+
+
+	else:
+		#set up variables
+		if varType[0]=='N':
+			varN = np.array(x,dtype=float)
+			varC = y
+			varNInd = 0
+			varCInd = 1
+		else:
+			varN = np.array(y,dtype=float)
+			varC = x
+			varNInd = 1
+			varCInd = 0
+
+		groups,unique = getGroups(varN,varC)
+		bp = plt.boxplot(groups)
+		plt.xticks(range(1,(len(unique)+1)),unique,rotation=45)
+		plt.xlabel(varName[varCInd])
+		plt.ylabel(varName[varNInd])
+	
+	if outfile=='':
+		plt.show()
+	else:
+		plt.savefig(outfile)
+		plt.clf()
+		plt.close()
+
+
+
+
+
 	
