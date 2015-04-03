bd83c62208da641675577b26296ed5ead807ab00
rtasseff <rtasseff@gmail.com>
2015-04-03 11:12:00 +0200

    minor changes to deal with errors, or difficulties found in use, ie plotting issues and NA values

--- statsUtil.py
+++ statsUtil.py
@@ -27,6 +27,9 @@ import operator as op
 import itertools
 import matplotlib.pyplot as plt
 
+nanValues = ['NA','NaN','na','nan']
+
+
 def iqr(x):
 	"""returns the inter quantile distance for
 	the variables in x.  
@@ -230,11 +233,10 @@ def makeConTable(x,y):
 	conTable = np.zeros((xN,yN))
 
 	for i in range(n):
-		if not np.isnan(xInt[i]) and not np.isnan(yInt[i]):
+		if xInt[i] >= 0 and yInt[i]>=0:
 			conTable[xInt[i],yInt[i]]+=1
 
 	return(conTable,xCat,yCat)
-	
 
 def getGroups(values,labels):
 	"""assuming the labels correspond to the values 
@@ -244,10 +246,13 @@ def getGroups(values,labels):
 	"""
 	unique = list(set(labels))
 	groups = []
+	cats = []
 	for label in unique:
-		groups.append(values[labels==label])
+		if label not in nanValues:
+			groups.append(values[labels==label])
+			cats.append(label)
 
-	return(groups,unique)
+	return(groups,cats)
 
 
 def cat2int(y):
@@ -256,27 +261,33 @@ def cat2int(y):
 	values of numbers from 0 to n-1
 	returns the new int array and list of cat labels.
 		a list of categories corresponding to int value
-	nan values (either 'nan' or the np object) will be preserved.
+	nan values (either 'nan' or the np object) will be preserved;
+	however int arrays cannot have nan so preserved as -1.
 	input array y must be an np array for indexing reasons.
+
+	as of now integers cannot have nan values, setting this to -1
 	"""
 	unique = list(set(y))
 	yNew = np.array(np.zeros(len(y)),dtype=int)
 	count = 0
+	cats = []
 	for i in range(len(unique)):
 		tmp = unique[i]
 		missing=False
 		if type(tmp)==np.string_:
-			if tmp.lower()=='nan':missing=True
+			if tmp in nanValues:missing=True
+			yNew[y==unique[i]] = -1
 		else: 
 			if np.isnan(tmp): missing=True
+			yNew[np.isnan(y)] = -1
 
-		if missing==False:
+		if not missing:
 			yNew[y==unique[i]] = count
 			count += 1
-		else:
-			yNew[y==unique[i]] = np.nan
+			cats.append(unique[i])
+			
 
-	return(yNew,unique)
+	return(yNew,cats)
 
 def softThresh(X,thresh):
 	"""Take X, and do soft 
@@ -611,6 +622,22 @@ def runPairwise(x,y,xType, yType, obsMinWarn=5, obsMinError=1):
 
 	return(r,p,obsMinErrorFlag)
 
+
+def qqPlot(pValues,outpath):
+	"""Make a qq plot of pValues
+	saved to output path, path should have png extension.
+	"""
+	n = len(pValues)
+	pExp = np.arange(1,0,-1.0/n)
+	plt.plot(pExp,np.sort(pValues)[::-1])
+	plt.plot([pExp[0],pExp[-1]],[pExp[0],pExp[-1]])
+	plt.xlabel('expected -logP')
+	plt.ylabel('observed -logP')
+	plt.savefig(outpath,format='png')
+	plt.clf()
+	plt.close()
+
+
 	
 	
 def plotPairwise(x,y,varType=['',''],varName=['',''],outfile=''):
@@ -620,16 +647,20 @@ def plotPairwise(x,y,varType=['',''],varName=['',''],outfile=''):
 	x	np str array, if type = N then this should be 
 		convertable to float array
 	y	same as x but for other variable
-	varType	tuple with 2 values to indicate type of x and y 
-		if left blank we assume x and y are lines form a 
+	varType	list with 2 values to indicate type of x and y 
+		if left blank we assume x and y are split lines form a 
 		feature matrix and the first entry is the label
+	varName list with 2 values to indicate name of x and y
+		if this *and* varType above is blank this will be taken as the 
+		label (first entry) of x and y.
+
 	"""
 	if varType[0]=='':
 		varType[0] = x[0].split(':')[0]
-		varName[0] = x[0].split(':')[-1]
+		if varName[0]=='':varName[0] = x[0]
 		x = x[1:]
 		varType[1] = y[0].split(':')[0]
-		varName[1] = y[0].split(':')[-1]
+		if varName[1]=='':varName[1] = y[0]
 		y = y[1:]
 
 	if not varType[0]=='N' and not varType[0]=='C' and not varType[0]=='B':
@@ -641,61 +672,96 @@ def plotPairwise(x,y,varType=['',''],varName=['',''],outfile=''):
 	if varType[0] == 'B': varType[0] = 'C' # no diff here
 	if varType[1] == 'B': varType[1] = 'C' # no diff here
 
+	
 	# check if both numerical:
 	if varType[0]=='N' and varType[1]=='N':
-		# scatter plot with line from least squares
-		A = np.vstack([x, np.ones(len(x))]).T
-		m, c = np.linalg.lstsq(A, y)[0]
+		xFloat = _getFloat(x)
+		yFloat = _getFloat(y)
 
-		plt.plot(np.array(x,dtype=float),np.array(y,dtype=float),'o',label='Data')
-		plt.plot(x, m*x + c, 'r', label='Fitted line')
-		plt.legend()
+		# must ignore all nan values
+		keep = np.array(np.ones(len(xFloat)),dtype=bool)
+		keep[np.isnan(xFloat)]=False
+		keep[np.isnan(yFloat)]=False
+		# non parametric tests used so lets consider ranks
+		xRank = rankdata(xFloat[keep])
+		yRank = rankdata(yFloat[keep])
+		#xRank = np.argsort(np.argsort(xFloat[keep]))
+		#yRank = np.argsort(np.argsort(yFloat[keep]))
 
-		plt.xlabel(varName[0])
-		plt.ylabel(varName[1])
+		plt.plot(xRank,yRank,'o',label='Data')
+		try:
+			# scatter plot with line from least squares
+			A = np.vstack([xRank, np.ones(len(xRank))]).T
+			m, c = np.linalg.lstsq(A, yRank)[0]
+			plt.plot(xRank, m*xRank + c, 'r', label='Fitted line')
+			plt.legend()
+
+		except ValueError as ve:
+			warnings.warn("Could not fit line for "+varName[0]+" vs "+varName[1]+": "+str(ve),UserWarning)
+		
+		
+		
+		plt.xlabel(varName[0]+' ranks')
+		plt.ylabel(varName[1]+' ranks')
+		#plt.ylabel(varName[1].split(':')[-1])
 	elif varType[0]=='C' and varType[1]=='C':
 		# right now we are just going with a simple 
 		# stacked bar plots, more advanced things
 		# like mosics are avalible in R or pythons statsmodels
 
 		conTable,xCat,yCat = makeConTable(x,y)
+		varInd = 0
+		# we want more bars less stacks so n shoudl be smallest
+		if len(xCat)>len(yCat):
+			tmpCat = yCat
+			yCat = xCat
+			xCat = tmpCat
+			conTable = conTable.T
+			varInd = 1
 		n,m = conTable.shape
+		# consider normalized contigency table
+		conTable = conTable/np.sum(conTable,0)
 		ind = np.arange(m)
 		bottomTmp = np.zeros(m)
 		colorList = ['b','g','r','c','y','m']
 		nColor = len(colorList)
 		colorInd = 0
 		for i in range(n):
-			plt.bar(ind,conTable[i],bottom=bottomTmp,color=colorList[colorInd],label=varName[0]+'::'+str(xCat[i]))
-			bottomTmp = conTable[i]
+			plt.bar(ind,conTable[i],bottom=bottomTmp,color=colorList[colorInd],label=varName[varInd]+'::'+str(xCat[i]))
+			bottomTmp += conTable[i]
 			colorInd +=1
 			if colorInd==nColor:colorInd=0
 
-		plt.ylabel('Count')
-		plt.xlabel(varName[1])
-		plt.xticks(ind,np.array(yCat,dtype=str),rotation=45)
+		plt.ylabel('Normalized Count')
+		plt.xlabel(varName[abs(varInd-1)])
+		plt.xticks(ind,np.array(yCat,dtype=str),rotation=10)
 		plt.legend()
 			
 
 
 	else:
+		#####
+		#fig = plt.figure(figsize=(8,3))
+
+		# number vs category 
 		#set up variables
 		if varType[0]=='N':
-			varN = np.array(x,dtype=float)
+			varN = _getFloat(x)
 			varC = y
 			varNInd = 0
 			varCInd = 1
 		else:
-			varN = np.array(y,dtype=float)
+			varN = _getFloat(y)
 			varC = x
 			varNInd = 1
 			varCInd = 0
 
 		groups,unique = getGroups(varN,varC)
 		bp = plt.boxplot(groups)
-		plt.xticks(range(1,(len(unique)+1)),unique,rotation=45)
+		plt.xticks(range(1,(len(unique)+1)),unique,rotation=10)
 		plt.xlabel(varName[varCInd])
 		plt.ylabel(varName[varNInd])
+		#plt.ylabel(varName[varNInd].split(':')[-1])
 	
 	if outfile=='':
 		plt.show()
@@ -706,6 +772,15 @@ def plotPairwise(x,y,varType=['',''],varName=['',''],outfile=''):
 
 
 
-
-
-	
+def _getFloat(x):
+	"""Create a float of this string"""
+	if x.dtype==np.dtype('float64'):xFloat = x
+	else:
+		# had issues with S2 dtypes cutting off the nan to na
+		xFloat = np.array(x,dtype='|S15')
+		for i in range(len(xFloat)):
+			if xFloat[i] in nanValues:
+				xFloat[i] = 'nan'
+		xFloat = np.array(xFloat,dtype=float)
+	return xFloat
+		
